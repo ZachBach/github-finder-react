ec26c6ebde146a12dc9ee14142bcb8b2c292b743
Zach Auerbach <auernachzach@gmail.com>
2020-05-17 15:18:48 -0700

    added enviroment variables for paths

--- src/App.js
+++ src/App.js
@@ -22,9 +22,15 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/user/:login' component={User} />
+                <Route path={process.env.PUBLIC_URL + '/'} component={Home} />
+                <Route
+                  path={process.env.PUBLIC_URL + 'about'}
+                  component={About}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + '/user/:login'}
+                  component={User}
+                />
                 <Route component={NotFound} />
               </Switch>
             </div>
