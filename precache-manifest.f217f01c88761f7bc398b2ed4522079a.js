e697ae582fc6f154cf1dc2b650282a2e031cf5d1
Zach Auerbach <auernachzach@gmail.com>
2023-04-24 10:43:04 -0700

    Updates

---
+++ precache-manifest.f217f01c88761f7bc398b2ed4522079a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbb8c6ff8ad2cfb1115d45464601bb6",
+    "url": "/github-finder-react/index.html"
+  },
+  {
+    "revision": "9e419dfde7993bd9cbf2",
+    "url": "/github-finder-react/static/css/main.8b94cd51.chunk.css"
+  },
+  {
+    "revision": "b1bae82a8f6a8f1d5be6",
+    "url": "/github-finder-react/static/js/2.a1ea335f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder-react/static/js/2.a1ea335f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e419dfde7993bd9cbf2",
+    "url": "/github-finder-react/static/js/main.ab994e49.chunk.js"
+  },
+  {
+    "revision": "c0006013c00f1d62b66a",
+    "url": "/github-finder-react/static/js/runtime-main.0846b5e1.js"
+  },
+  {
+    "revision": "baeaf0c7f02777839e58eed62cc714c4",
+    "url": "/github-finder-react/static/media/spinner.baeaf0c7.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0839a9a663c80ff8c6f7cac3e864cf81.js"
+  "/github-finder-react/precache-manifest.f217f01c88761f7bc398b2ed4522079a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-finder-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
